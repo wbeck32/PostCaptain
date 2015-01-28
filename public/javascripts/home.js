6e35ba3345045e71f6f54a17f0be38e5542ec879
Wendy Beck <webeck@gmail.com>
2015-01-28 14:30:00 -0800

    updates and clean ups

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,29 +1,10 @@
 (function(){
 
-
-//need jquery, node
-
-var cSide = $.ajax({
+$.ajax({
 	url: 'http://localhost:3000/home'
 })
 .done(function(data) {
-	console.log(data);
-	//data.join('');
 	$(data).appendTo('#projects');
-//	return data;
 });
-//console.log(cSide);
-
-//create new project div
-
-//for each file in project
-//	output contents of html file
-//	projDiv += contents
-//end foreach
-
-//append projDiv to main 
-
-
-
 
 })();
--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,48 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var config = require('../config.json');
 var __dirname = './public/projects/';
 var fText = [];
 var fileNames = [];
+
 var fileReader = function (index, file) {
 	fs.readFile(file, function(err, data){
 		fText[index] = data;
-		index++;	
+		index++;
 		interator(index);
 	});
-}
-var interator = function(index){				
-		if (index < fileNames.length) {
-			
-			fileReader(index,__dirname+fileNames[index]);
-		}
-		else{
-			router.get('/home', function(req,res,next){ 
-				res.render('home', { title: 'These projects are awesome', content: fText.join('')});
-			});
-			
-		}
+};
+
+var interator = function(index){
+	if (index < fileNames.length) {	
+		fileReader(index,__dirname+fileNames[index]);
 	}
+	else{
+        router.get('/home', function(req,res,next){ 
+			res.render('home', { content: fText.join('')});
+		});
+	}
+};
 
 var fileTrawler = function(){
 	fs.readdir(__dirname, function(err,files) {		
-		fileNames = files;
+    fileNames = files;
 		if (err){
 			throw err;
 		} 
 		else {
-				var index = 0
-				fileReader(index, __dirname+fileNames[index]);
+			var index = 0;
+			fileReader(index, __dirname+fileNames[index]);
 		}		
-
 	});
-}
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'I am awesome.' });
+	fileTrawler();
+  	res.render('index', { title: config.locals.name, desc: config.locals.description });
 });
-fileTrawler();
 
 
 module.exports = router;
