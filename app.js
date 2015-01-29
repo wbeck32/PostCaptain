c6819aa4762cf5402afbd542a8026e9f904d9b9d
Russell DuBrock <russell.dubrock@gmail.com>
2015-01-29 10:25:52 -0800

    actually has directory copy now

--- app.js
+++ app.js
@@ -4,12 +4,23 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var copy = require('directory-copy');
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
 
 var app = express();
 
+copy(
+    { src: __dirname+'/PostCaptain/public'
+    , dest: __dirname
+    , excludes: [/^\./]
+    }
+   , function () {
+    console.log('done!')
+})
+.on('log', function (ms, level){
+    console.log(leve + ': ' + msg)
+})
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -23,7 +34,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
