b79107520a3daf757c152e1680610d42335e9d81
Wendy Beck <webeck@gmail.com>
2015-01-30 15:36:46 -0800

    I do not even know any more

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var routes = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', './public/views');
+app.set('views', path.join('__dirname','/public/views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
