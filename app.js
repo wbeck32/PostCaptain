84478cb71a45d4541d45a89af38b8ec8c8d104cc
Wendy Beck <webeck@gmail.com>
2015-01-30 14:19:22 -0800

    pathologywq!

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-
+console.log(__dirname);
 var routes = require('./routes/index');
 
 var app = express();
